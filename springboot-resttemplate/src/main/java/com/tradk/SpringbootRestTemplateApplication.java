4eed5cb64b26b501be4974f424f5e1fd8f4407f9
tradk <tradk1995@gmail.com>
2019-03-25 22:34:45 +0700

    Initial SpringBoot vs RestTemplate project

---
+++ springboot-resttemplate/src/main/java/com/tradk/SpringbootRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.tradk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestTemplateApplication.class, args);
+    }
+
+}

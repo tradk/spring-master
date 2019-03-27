ba95d33fb48d157669fd1ce7365cfa2226264b94
tradk <tradk1995@gmail.com>
2019-03-27 23:40:09 +0700

    Update get product list

--- springboot-resttemplate/src/main/java/com/tradk/controller/ConsumeWebService.java
+++ springboot-resttemplate/src/main/java/com/tradk/controller/ConsumeWebService.java
@@ -1,15 +1,15 @@
 package com.tradk.controller;
 
-import com.tradk.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
-import java.util.List;
 
 @RestController
 public class ConsumeWebService {
@@ -18,15 +18,14 @@ public class ConsumeWebService {
     RestTemplate restTemplate;
 
     @RequestMapping(value = "/template/products")
-    public ResponseEntity<List<Product>> getProductList() {
+    public String getProductList() {
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<List<Product>> entity = new HttpEntity<>(headers);
-        ResponseEntity<List<Product>> response = restTemplate.exchange(
-                "http://localhost:3000/products",
-                HttpMethod.GET,
-                entity,
-                new ParameterizedTypeReference<List<Product>>() {});
-        return response;
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        return restTemplate.exchange(
+            "http://localhost:3000/products",
+            HttpMethod.GET,
+            entity,
+            String.class).getBody();
     }
 }

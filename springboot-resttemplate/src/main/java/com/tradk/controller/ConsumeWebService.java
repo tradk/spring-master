417b24b88431a2cea181e162a2dd36bc5d668f7b
tradk <tradk1995@gmail.com>
2019-03-28 22:39:47 +0700

    Add func access Product db

--- springboot-resttemplate/src/main/java/com/tradk/controller/ConsumeWebService.java
+++ springboot-resttemplate/src/main/java/com/tradk/controller/ConsumeWebService.java
@@ -1,15 +1,13 @@
 package com.tradk.controller;
 
+import com.tradk.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
 
 @RestController
 public class ConsumeWebService {
@@ -17,15 +15,64 @@ public class ConsumeWebService {
     @Autowired
     RestTemplate restTemplate;
 
+    /**
+     * Get list data
+     */
     @RequestMapping(value = "/template/products")
-    public String getProductList() {
+    public String getProducts() {
         HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> entity = new HttpEntity<>(headers);
+
         return restTemplate.exchange(
-            "http://localhost:3000/products",
-            HttpMethod.GET,
-            entity,
-            String.class).getBody();
+                "http://localhost:3000/products",
+                HttpMethod.GET, entity,
+                String.class).getBody();
     }
+
+    /**
+     * Add new product
+     */
+    @RequestMapping(value = "/template/products", method = RequestMethod.POST)
+    public String createProduct(@RequestBody Product product) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Product> entity = new HttpEntity<>(product, headers);
+
+        return restTemplate.exchange(
+                "http://localhost:3000/products",
+                HttpMethod.POST, entity,
+                String.class).getBody();
+    }
+
+    /**
+     * Update product
+     */
+    @RequestMapping(value = "/template/products/{id}", method = RequestMethod.PUT)
+    public String updateProduct(@PathVariable("id") int id, @RequestBody Product product) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Product> entity = new HttpEntity<>(product, headers);
+
+        return restTemplate.exchange(
+                String.format("http://localhost:3000/products/%d", id),
+                HttpMethod.PUT, entity,
+                String.class).getBody();
+    }
+
+    /**
+     * Delete product
+     */
+    @RequestMapping(value = "/template/products/{id}", method = RequestMethod.DELETE)
+    public String deleteProduct(@PathVariable("id") int id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Product> entity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(
+                String.format("http://localhost:3000/products/%d", id),
+                HttpMethod.DELETE, entity,
+                String.class).getBody();
+    }
+
 }
--- springboot-resttemplate/src/main/java/com/tradk/model/Product.java
+++ springboot-resttemplate/src/main/java/com/tradk/model/Product.java
@@ -1,16 +1,67 @@
 package com.tradk.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-import java.io.Serializable;
-
-@AllArgsConstructor
-@Data
-public class Product implements Serializable {
+public class Product {
     private int id;
     private String name;
     private String category;
     private String status;
     private int price;
+
+    public Product() {
+    }
+
+    public Product(String name, String category, String status, int price) {
+        this.name = name;
+        this.category = category;
+        this.status = status;
+        this.price = price;
+    }
+
+    public Product(int id, String name, String category, String status, int price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.status = status;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }

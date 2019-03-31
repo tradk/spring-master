3c38c59b145c357e6560160908790e1c7ad06aff
tradk <tradk1995@gmail.com>
2019-04-01 01:56:36 +0700

    Config security in application

--- springboot-websocket/src/main/java/com/tradk/config/WebSecurityConfig.java
+++ springboot-websocket/src/main/java/com/tradk/config/WebSecurityConfig.java
@@ -1,4 +1,34 @@
 package com.tradk.config;
 
-public class WebSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("user123").password(encoder.encode("user123")).roles("USER").and()
+                .withUser("admin123").password(encoder.encode("admin123")).roles("ADMIN", "USER");
+    }
+
 }

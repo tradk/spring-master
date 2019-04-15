7731e44691b05b6367c6fed8e5b3d321f16b6c0c
tradk <tradk1995@gmail.com>
2019-04-15 20:40:13 +0700

    Initial project

---
+++ springboot-rest-api-jwt/src/main/java/com/tradk/SpringbootRestApiJwtApplication.java
@@ -0,0 +1,13 @@
+package com.tradk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApiJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestApiJwtApplication.class, args);
+    }
+
+}

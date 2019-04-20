96debcd1dbc20b7dc336ac5ee935b2db3d81136c
tradk <tradk1995@gmail.com>
2019-04-21 00:11:06 +0700

    Add UserRepository

---
+++ springboot-rest-api-jwt/src/main/java/com/tradk/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.tradk.repository;
+
+import com.tradk.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.transaction.Transactional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    boolean existsByUsername(String username);
+
+    User findByUsername(String username);
+
+    @Transactional
+    void deleteByUsername(String username);
+
+}
